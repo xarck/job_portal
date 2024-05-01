e3fd9080e8db4e58901706a39d0393bb453c2c35
Ashish <iamashking123@gmail.com>
2024-05-01 21:34:43 +0530

    redux store created

---
+++ src/store.js
@@ -0,0 +1,33 @@
+import { createStore } from "redux";
+
+const initialState = {
+    jobListings: [],
+    filters: {
+        minExperience: 0,
+        companyName: "",
+        location: "",
+        remote: null,
+        techStack: [],
+        role: "",
+        minBasePay: 0,
+    },
+    currentPage: 1,
+};
+
+const reducer = (state = initialState, action) => {
+    switch (action.type) {
+        case "SET_JOB_LISTINGS":
+            console.log(action.payload);
+            return { ...state, jobListings: action.payload };
+        case "SET_FILTERS":
+            return { ...state, filters: action.payload };
+        case "SET_CURRENT_PAGE":
+            return { ...state, currentPage: action.payload };
+        default:
+            return state;
+    }
+};
+
+const store = createStore(reducer);
+
+export default store;
